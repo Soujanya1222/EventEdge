9b4e7ce0fc5227ac894b59e36743d2eed4cb281a
soujanya parananti <paranantisoujanya@gmail.com>
2025-12-20 19:39:51 +0530

    updated files

--- frontend/event-tracking-app/src/pages/Admin Page/AdminDashboard.jsx
+++ frontend/event-tracking-app/src/pages/Admin Page/AdminDashboard.jsx
@@ -39,17 +39,17 @@ export default function AdminDashboard(props){
         return <p>Loading...</p>
     }
     return(
-        <div className=".dashboard-container">
-            <h1 className="page-title"><strong>Admin Dashboard</strong></h1>
-            <p>Name--{user.name}</p>
+        <div className="dashboard-container">
+            <h1 className="dashboard-header"><strong>Admin Dashboard</strong></h1>
+            <p className="dashborad-header">Welcome,<strong>{user.name}</strong></p>
             <div className="stats-container">
-                <div className="stat-card">
+                <div className="dashboard-card organiser">
                     <h3>Total Organisers</h3>
                     <p>{organisers.length}</p>
                     <button className="view-btn" onClick={()=>navigate("/organiserList")}>View Organiser</button>
                 </div>
 
-                <div className="stat-card">
+                <div className="dashboard-card events">
                     <h3>Total Events</h3>
                     <p>{events.length}</p>
                     <button className="view-btn" onClick={()=>{
@@ -58,7 +58,7 @@ export default function AdminDashboard(props){
 
                 </div>
 
-                <div className="stat-card">
+                <div className="dashboard-card users">
                     <h3>Total Users</h3>
                     <p>{users.length}</p>
                     <button className="view-btn" onClick={()=>{
@@ -67,20 +67,20 @@ export default function AdminDashboard(props){
 
                 </div>
                 
-                <div className="stat-box">
+                <div className="dashboard-card pending">
                 <p>Pending Events</p>
                 <h2>{pendingCount}</h2>
                  <button className="view-btn" onClick={()=>navigate("/organiser/events?status=pending")}>View pending Events</button>
                 </div>
                 
-                <div className="stat-box">
+                <div className="dashboard-card approved">
                 <p>Approved Events</p>
                 <h2>{approvedCount}</h2>
                  <button className="view-btn" onClick={()=>navigate("/organiser/events?status=approved")}>View Approved Evets</button>
                 </div>
 
 
-                 <div className="stat-box">
+                 <div className="dashboard-card rejected">
                 <p>Rejected Events</p>
                 <h2>{rejectedCount}</h2>
                  <button className="view-btn" onClick={()=>navigate("/organiser/events?status=rejected")}>View Rejected Events</button>
